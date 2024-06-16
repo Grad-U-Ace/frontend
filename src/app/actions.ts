46585c1ba6dbc91f4063ce29c7626610a8050e37
Imo Madjid <mhmmadjid@gmail.com>
2024-06-16 07:35:27 +0700

    feat: added ai responses

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,3 +8,45 @@ export async function fetchEcommerceData() {
   const data = await response.json();
   return data;
 }
+
+export async function fetchAIAnswer({
+  ecommerce,
+  product,
+  message,
+  reply,
+}: {
+  ecommerce: string;
+  product: string;
+  message: string;
+  reply: string;
+}) {
+  const response = await fetch(
+    `http://127.0.0.1:8000/ecommerce/${ecommerce}/product/${product}/message/${message}/reply/${reply}/ask`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  return data;
+}
+
+export async function fetchTranslation({
+  ecommerce,
+  product,
+  message,
+  reply,
+}: {
+  ecommerce: string;
+  product: string;
+  message: string;
+  reply: string;
+}) {
+  const response = await fetch(
+    `http://127.0.0.1:8000/ecommerce/${ecommerce}/product/${product}/message/${message}/reply/${reply}/translate`,
+  );
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  const data = await response.json();
+  return data;
+}
--- src/app/components/MessageDrawer.tsx
+++ src/app/components/MessageDrawer.tsx
@@ -1,7 +1,7 @@
+import { olshopAtom } from "@/atoms";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
-  DrawerClose,
   DrawerContent,
   DrawerDescription,
   DrawerFooter,
@@ -12,10 +12,17 @@ import {
 import { TableCell, TableRow } from "@/components/ui/table";
 import { Textarea } from "@/components/ui/textarea";
 import { flexRender, Row } from "@tanstack/react-table";
+import { useAtom } from "jotai";
+import { useState } from "react";
+import { fetchAIAnswer, fetchTranslation } from "../actions";
 import type { Message } from "../types";
 
 export default function MessageDrawer({ row }: { row: Row<any> }) {
+  const [olshop] = useAtom(olshopAtom);
   const message: Message = row.original;
+  const [answer, setAnswer] = useState<string>("");
+  const [translation, setTranslation] = useState<string>("");
+
   return (
     <Drawer shouldScaleBackground>
       <DrawerTrigger asChild>
@@ -36,7 +43,7 @@ export default function MessageDrawer({ row }: { row: Row<any> }) {
           <DrawerTitle className="px-5 text-3xl text-white">
             {message.customer_name}
           </DrawerTitle>
-          <DrawerDescription className="p-5">
+          <DrawerDescription className="flex flex-col gap-4 p-5">
             <div className="text-zinc-300">
               {message.replies.map((reply, index) => (
                 <div key={index}>
@@ -49,9 +56,64 @@ export default function MessageDrawer({ row }: { row: Row<any> }) {
                 </div>
               ))}
             </div>
-            <Button className="bg-teal-500 shadow-[inset_0_1px_2px_0_rgba(0,0,0,0.05)] shadow-teal-200 hover:bg-teal-600 hover:shadow-teal-300">
-              Ask AI
-            </Button>
+            <div className="flex items-center gap-2">
+              <Button
+                className="border border-teal-500 bg-teal-500 shadow-[inset_0_1px_2px_0_rgba(0,0,0,0.05)] shadow-teal-200 hover:border-teal-600 hover:bg-teal-600 hover:shadow-teal-300"
+                onClick={async () => {
+                  const ecommerce = olshop;
+                  const product = message.product_id.toString();
+                  const messageId = message.id.toString();
+                  const reply =
+                    message.replies[message.replies.length - 1].id.toString();
+                  const answer = await fetchAIAnswer({
+                    ecommerce,
+                    product,
+                    message: messageId,
+                    reply,
+                  });
+                  console.log(reply);
+                  console.log(answer);
+                  setAnswer(answer.content);
+                }}
+              >
+                Tanya AI
+              </Button>
+              <Button
+                className="border border-zinc-500 bg-zinc-500 shadow-[inset_0_1px_2px_0_rgba(0,0,0,0.05)] shadow-zinc-400 hover:border-zinc-600 hover:bg-zinc-600 hover:shadow-zinc-400"
+                onClick={async () => {
+                  console.log("Terjemahkan");
+                  const ecommerce = olshop;
+                  const product = message.product_id.toString();
+                  const messageId = message.id.toString();
+                  const reply = message.replies[0].id.toString();
+                  const translation = await fetchTranslation({
+                    ecommerce,
+                    product,
+                    message: messageId,
+                    reply,
+                  });
+                  console.log(translation);
+                  setTranslation(translation.content);
+                }}
+              >
+                Terjemahkan
+              </Button>
+              <Button className="border border-zinc-500 bg-zinc-500 shadow-[inset_0_1px_2px_0_rgba(0,0,0,0.05)] shadow-zinc-400 hover:border-zinc-600 hover:bg-zinc-600 hover:shadow-zinc-400">
+                Rangkumkan
+              </Button>
+            </div>
+            {/* if answer is not null, then display a teal div */}
+            {answer && (
+              <div className="rounded-xl bg-teal-500 p-5">
+                <p className="text-white">{answer}</p>
+              </div>
+            )}
+            {/* if translation is not null, then display a zinc div */}
+            {translation && (
+              <div className="rounded-xl bg-zinc-500 p-5">
+                <p className="text-white">{translation}</p>
+              </div>
+            )}
           </DrawerDescription>
           <Textarea className="border-none bg-zinc-950 text-zinc-300 shadow-[inset_0_-1px_3px_-1px_rgba(0,0,0,0.05)] shadow-white/70 focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0" />
         </DrawerHeader>
